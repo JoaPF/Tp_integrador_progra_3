f4ed84d50c7ce3961d360a92300ef5d368fb4818
Joa <joaquinperezfigueira@gmail.com>
2026-07-03 05:03:22 -0300

    Creados 2 endpoints minimos para consultar usuarios (/api/users) y productos (/api/products) y la conexion a la bbdd

--- tpintegradorBack/index.js
+++ tpintegradorBack/index.js
@@ -1,8 +1,7 @@
 import express from "express";
 const app = express();
 import enviroments from "./src/api/config/enviroments.js";
-
-
+import connection from "./src/api/database/db.js";
 
 const PORT = enviroments.port;
 
@@ -11,10 +10,37 @@ app.get("/", (req, res) => {
     res.send("Hola mundo");
 });
 
-app.get("/api/products", (req, res) => {
-    res.send("Aca devolveremos el JSON que me devuelva la consulta SELECT * FROM products");
-});
-
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
+
+//endpoint para devolver TODOS los productos
+//devuelve el resultado de la consulta a la bbdd y el estatus 200(ok)
+app.get("/api/products", async (req, res) => {
+    try{
+        const sql = "SELECT * FROM productos";
+        const [rows] = await connection.query(sql);
+
+        res.status(200).json({rows});
+        console.log(rows);
+
+    } catch (error){
+        console.log("Error: ", error.message);
+        
+    }
+});
+
+//endpoint para devolver TODOS los usuarios
+app.get("/api/users", async (req, res) => {
+    try{
+        const sql = "SELECT * FROM usuario";
+        const [rows] = await connection.query(sql);
+
+        res.status(200).json({rows});
+        console.log(rows);
+
+    } catch (error){
+        console.log("Error: ", error.message);
+        
+    }
 });
--- tpintegradorBack/src/api/database/db.js
+++ tpintegradorBack/src/api/database/db.js
@@ -1,2 +1,17 @@
 import mysql2 from "mysql2/promise";
 
+// Importamos la informacion de la conexion a la BBDD
+import environments from "../config/enviroments.js";
+
+// Extraemos solo el objeto database
+const { database } = environments;
+
+//pool de conexiones a la bbdd
+const connection = mysql2.createPool({
+    host: database.host,
+    database: database.name,
+    user: database.user,
+    password: database.password
+});
+
+export default connection;
